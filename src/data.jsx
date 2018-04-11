75b6d89ae5b9cb0d577e8e6cf50bc0289d9c6811
hvmitsui <33299535+hvmitsui@users.noreply.github.com>
2018-04-10 22:12:23 -1000

    Update data.jsx

--- src/data.jsx
+++ src/data.jsx
@@ -37,7 +37,7 @@ export default {
     {
       course: "Math 241/251",
       desc: "Calc 1",
-      pre: "",
+      pre: "Placement Exam or MATH 140",
       resources: [
           "http://ocw.uci.edu/courses/math_2a_calculus_i.html",
           "http://ocw.uci.edu/courses/math_2b_calculus.html",
@@ -47,14 +47,44 @@ export default {
     {
       course: "Math 242/252",
       desc: "Calc 2",
-      pre: "",
+      pre: "MATH 241/251A",
       resources: [
         "http://ocw.uci.edu/courses/math_2a_calculus_i.html",
         "http://ocw.uci.edu/courses/math_2b_calculus.html",
         "https://ocw.mit.edu/courses/mathematics/18-01-single-variable-calculus-fall-2006/"
       ]
     },
-
+    {
+      course: "Math 243/253A",
+      desc: "Calc 3",
+      pre: "MATH 242/252A",
+      resources: [
+        "http://ocw.uci.edu/courses/math_2a_calculus_i.html",
+        "http://ocw.uci.edu/courses/math_2b_calculus.html",
+        "https://ocw.mit.edu/courses/mathematics/18-02-multivariable-calculus-fall-2007/"
+      ]
+    },
+    {
+      course: "Math 244/253A",
+      desc: "Calc 4",
+      pre: "MATH 243/252A",
+      resources: [
+        "http://ocw.uci.edu/courses/math_2a_calculus_i.html",
+        "http://ocw.uci.edu/courses/math_2b_calculus.html",
+        "https://ocw.mit.edu/courses/mathematics/18-02-multivariable-calculus-fall-2007/"
+      ]
+    },
+    {
+      course: "Math 307",
+      desc: "Linear Algebra",
+      pre: "MATH 243/253A (concurr.)",
+      resources: [
+        "https://ocw.mit.edu/courses/mathematics/18-06-linear-algebra-spring-2010/video-lectures/",
+        "https://www.youtube.com/watch?v=kjBOesZCoqc&list=PLZHQObOWTQDPD3MizzM2xVFitgF8hE_ab",
+        "https://www.khanacademy.org/math/linear-algebra",
+        "https://www.khanacademy.org/math/differential-equations"
+      ]
+    },    
   ],
   physics: [
     {
