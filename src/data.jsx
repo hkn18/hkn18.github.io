8159b3a2d5db13f9e257ae09c3a480c2ce5ed84b
hvmitsui <33299535+hvmitsui@users.noreply.github.com>
2018-04-10 22:23:27 -1000

    Update data.jsx

--- src/data.jsx
+++ src/data.jsx
@@ -88,13 +88,51 @@ export default {
   ],
   physics: [
     {
-      course: "Math 241/251",
-      desc: "Calc 1",
-      pre: "",
+      course: "Phys 170",
+      desc: "Mechanics/kinematics",
+      pre: "MATH 242/252A (concurr.)",
       resources: [
-        "http://ocw.uci.edu/courses/math_2a_calculus_i.html",
-        "http://ocw.uci.edu/courses/math_2b_calculus.html",
-        "https://ocw.mit.edu/courses/mathematics/18-01-single-variable-calculus-fall-2006/"
+        "http://ocw.uci.edu/courses/physics_3a_basic_physics.html",
+        "https://www.khanacademy.org/science/physics"
+      ]
+    },
+    {
+      course: "Phys 272",
+      desc: "Electromagnetics/geometric optics",
+      pre: "PHYS 170, MATH 242/252A",
+      resources: [
+        "http://ocw.uci.edu/courses/physics_3b_basic_physics_ii.html",
+        "https://www.khanacademy.org/science/physics"
+      ]
+    },
+    {
+      course: "Phys 274",
+      desc: "Qunatum mechanics",
+      pre: "PHYS 272, MATH 243/253A (concurr.)",
+      resources: [
+        "http://ocw.uci.edu/courses/physics_3c_basic_physics_iii.html",
+        "https://www.khanacademy.org/science/physics"
+      ]
+    },
+  ]
+  chemistry: [
+    {
+      course: "CHEM 161",
+      desc: "Gen Chem 1",
+      pre: "Placement Exam",
+      resources: [
+        "http://ocw.uci.edu/courses/chem_1a_general_chemistry.html",
+        "http://ocw.uci.edu/courses/chem_1b_general_chemistry.html",
+        "https://www.khanacademy.org/science/chemistry"
+      ]
+    },
+    {
+      course: "CHEM 162",
+      desc: "Gen Ghem 2",
+      pre: "CHEM 161",
+      resources: [
+        "http://ocw.uci.edu/courses/chem_1c_general_chemistry.html",
+        "https://www.khanacademy.org/science/chemistry"
       ]
     },
   ]
